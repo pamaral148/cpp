2cc699eaf8093b6c9cf4292a24b09f34141cfc30
Paulo Amaral <pamaral@monexa.com>
2014-11-13 15:10:58 -0800

    added assign 07

---
+++ assignment07/assignment07/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment07
+//
+//  Created by Paulo Amaral on 2014-11-13.
+//  Main function for assignment06 application
+//
+
+#include <iostream>
+
+int main() {
+    // start coding
+    return 0;
+}
