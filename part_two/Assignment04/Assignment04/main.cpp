f19380b13ebac1de579d21d427800eba80312e1d
pamaral148 <pamaral148@gmail.com>
2015-02-18 19:29:56 -0800

    created project for assignment 04

---
+++ part_two/Assignment04/Assignment04/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment04
+//
+//  Created by Paulo Amaral on 2015-02-18.
+//  Driver file for Comp2618 assignment 04
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
