85eba4af4f582a6b37cb8c514248feeb3ff68963
pamaral148 <pamaral148@gmail.com>
2015-02-20 18:41:28 -0800

    completed validations and structure

--- part_two/Assignment04/Assignment04/PhoneNumber.cpp
+++ part_two/Assignment04/Assignment04/PhoneNumber.cpp
@@ -8,21 +8,85 @@
 
 #include "PhoneNumber.h"
 
-const int ERROR = 1;
-
 // default ctor
 PhoneNumber::PhoneNumber() noexcept
+{ /* empty ctor */ }
+
+// validates area code input
+bool PhoneNumber::validateAreaCode() const noexcept
 {
-    // TODO ctor
+    const short start = 1;
+    bool test;
+    if(phone[startBracketIndex] != openBracket ||
+       phone[endBracketIndex] != closeBracket  ||
+       phone[spaceIndex] != space ||
+       phone[dashIndex] != '-' ||
+       phone[start] == zero ||
+       phone[start] == one ||
+      (phone[areaCodeMiddle] != zero &&
+       phone[areaCodeMiddle] != one)
+    ) {
+        test = false;
+    } else {
+        test = true;
+    }
+    return test;
+}
+
+// validates exchange input
+bool PhoneNumber::validateExchange() const noexcept
+{
+    bool test;
+    if(phone[exchangeStart] == zero ||
+       phone[exchangeStart] == one
+    ) {
+        test = false;
+    } else {
+        test = true;
+    }
+    return test;
+}
+
+// sets the area code
+void PhoneNumber::setAreaCode() noexcept
+{
+    const short max = 4;
+    short j = 0;
+    for (short i = areaCodeStart; i < max; ++i, ++j) {
+        areaCode[j] = phone[i];
+    }
+    areaCode[++j] = nullChar;
+}
+
+// sets the exchange
+void PhoneNumber::setExchange() noexcept
+{
+    const short max = 9;
+    short j = 0;
+    for (short i = exchangeStart; i < max; ++i, ++j) {
+        exchange[j] = phone[i];
+    }
+    exchange[++j] = nullChar;
+}
+
+void PhoneNumber::setLine() noexcept
+{
+    const short max = 15;
+    short j = 0;
+    for (short i = lineStart; i < max; ++i, ++j) {
+        line[j] = phone[i];
+    }
+    line[++j] = nullChar;
 }
 
 // overloaded insertion operator
 // inserts phone number to ostream in formatted output
 std::ostream& operator<<( std::ostream& os, const PhoneNumber& phoneNumber ) noexcept
 {
-    std::cout << '(' << phoneNumber.areaCode << ") "
-              << phoneNumber.exchange
-              << '-' << phoneNumber.line;
+    std::cout << phoneNumber.openBracket << phoneNumber.areaCode
+              << phoneNumber.closeBracket << phoneNumber.space
+              << phoneNumber.exchange << phoneNumber.dash
+              << phoneNumber.line;
     return os;
 }
 
@@ -31,31 +95,25 @@ std::istream& operator>>( std::istream& is, PhoneNumber& phoneNumber ) noexcept
 {
     // char location constants
     const int size = 15,
-              openBracket = 0,
-              closeBracket = 4,
-              space = 5,
-              dash = 9;
+              charCount = 14;
     
     is.get(phoneNumber.phone, size, '\n');
     
-    if(phoneNumber.phone[openBracket] != '('  ||
-       phoneNumber.phone[closeBracket] != ')' ||
-       phoneNumber.phone[space] != ' '        ||
-       phoneNumber.phone[dash] != '-'
+    if(is.gcount() != charCount ||
+       !phoneNumber.validateAreaCode() ||
+       !phoneNumber.validateExchange()
     ) {
+        // set istream failbit
+        is.clear(std::ios_base::failbit);
+    }
+    if(is.fail()) {
         std::cerr << "Invalid phone number entered...\n";
-        exit(ERROR);
+        exit(errno);
     } else {
-        for (int i = 1, j = 0, k = 6; i < 4; ++i, ++j, ++k) {
-            phoneNumber.areaCode[j] = phoneNumber.phone[i];
-            phoneNumber.exchange[j] = phoneNumber.phone[k];
-        }
-        for (int i = 10, j = 0; i < 15; ++i, ++j) {
-            phoneNumber.line[j] = phoneNumber.phone[i];
-        }
-        phoneNumber.areaCode[3] = '\0';
-        phoneNumber.exchange[3] = '\0';
-        phoneNumber.line[4] = '\0';
+        phoneNumber.setAreaCode();
+        phoneNumber.setExchange();
+        phoneNumber.setLine();
     }
     return is;
 }
+
--- part_two/Assignment04/Assignment04/PhoneNumber.h
+++ part_two/Assignment04/Assignment04/PhoneNumber.h
@@ -19,6 +19,28 @@ class PhoneNumber
 public:
     PhoneNumber() noexcept; // default constructor
 private:
+    // char constants for validation and output
+    const static char openBracket = '(',
+                      closeBracket = ')',
+                      space = ' ',
+                      dash = '-',
+                      zero = '0',
+                      one = '1',
+                      nullChar = '\0';
+    // index constants
+    const static short startBracketIndex = 0,
+                       endBracketIndex = 4,
+                       spaceIndex = 5,
+                       dashIndex = 9,
+                       exchangeStart = 6,
+                       areaCodeMiddle = 2,
+                       lineStart = 10,
+                       areaCodeStart = 1;
+    bool validateAreaCode() const noexcept; // validates area code input
+    bool validateExchange() const noexcept; // validates exchange input
+    void setAreaCode() noexcept; // sets the area code
+    void setExchange() noexcept; // sets the exchange
+    void setLine() noexcept; // sets the line
     char phone[ 15 ]; // holds phone number
     char areaCode[ 4 ]; // holds area code
     char exchange[ 4 ]; // holds exchange
